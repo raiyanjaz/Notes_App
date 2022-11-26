59db6bf9bbbdd003e6cb5c6af6d543aa3a71a82b
Raiyan Aaijaz <raiyanjaz@gmail.com>
2022-11-26 18:00:09 -0500

    Updated CSS

--- notesapp/src/sidebar.js
+++ notesapp/src/sidebar.js
@@ -1,7 +1,23 @@
 function sidebar() {
     return (
-        <div className='app-sidebar'>
-        
+        <div className="app-sidebar">
+            <div className="app-sidebar-header">
+                <h1>Notes</h1>
+                <button> Add </button>
+            </div>
+            <div className="app-sidebar-notes">
+                <div className="app-sidebar-note">
+                    <div className="sidebar-note-title">
+                        <strong> Title </strong>
+                        <button> Delete </button>
+                    </div>
+                    <p> Note Preview </p>
+                    <small className="note-meta"> 
+                        Last modified [insert_date]
+                    </small>
+                </div>
+            </div>
+
         </div>
     )
 }
